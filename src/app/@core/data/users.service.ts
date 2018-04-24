7329bf383836dbdcb6b678152a2d9ddd4a30e817
Abhishek Bhattacharya <abhattacharya@tycoint.com>
2018-04-24 10:04:44 +0530

    Changes

--- src/app/@core/data/users.service.ts
+++ src/app/@core/data/users.service.ts
@@ -9,6 +9,7 @@ export class UserService {
 
   private users = {
     nick: { name: 'Nick Jones', picture: 'assets/images/nick.png' },
+    abhi: { name: 'Abhishek', picture: 'assets/images/abhi.png' },
     eva: { name: 'Eva Moor', picture: 'assets/images/eva.png' },
     jack: { name: 'Jack Williams', picture: 'assets/images/jack.png' },
     lee: { name: 'Lee Wong', picture: 'assets/images/lee.png' },
--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { }
-
 @Component({
   selector: 'ngx-tables',
   template: `<router-outlet></router-outlet>`,
